3659b8fb805b9b68fe5d0279a42388d2fac10bb2
Imran Hossen <imranhossen20@gmail.com>
2022-03-01 18:13:17 +0600

    third

--- js/mobile.js
+++ js/mobile.js
@@ -6,7 +6,23 @@ const searchMobile = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayResult(data.data));
+}
+const displayResult = data => {
+    const searchResult = document.getElementById('search-result')
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card">
+                <img src="${data.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${data.brand}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in toadditional content. This content is a little bit longer.</p>
+                </div>
+            </div>`;
+        searchResult.appendChild(div)
 
-
+    });
 }
