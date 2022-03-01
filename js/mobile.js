844637c38290bc61c224637a9dc7fcecdc209278
Imran Hossen <imranhossen20@gmail.com>
2022-03-01 16:15:24 +0600

    second

--- js/mobile.js
+++ js/mobile.js
@@ -1,1 +1,12 @@
-console.log(imran)
+const searchMobile = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
+
+
+}
