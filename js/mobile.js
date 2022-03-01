cceaa05112d1539ad7189adf9081d79ac32e9691
Imran Hossen <imranhossen20@gmail.com>
2022-03-02 00:53:52 +0600

    commit

--- js/mobile.js
+++ js/mobile.js
@@ -6,20 +6,24 @@ const searchMobile = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => displayResult(data.data));
+        .then(data => displayResult(data.data.slice(0, 20)));
 }
 const displayResult = data => {
+    console.log(data)
     const searchResult = document.getElementById('search-result')
     data.forEach(data => {
-        console.log(data);
+
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
             <div class="card">
-                <img src="${data.image}" class="card-img-top" alt="...">
+                <img height= 300px src="${data.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${data.brand}</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in toadditional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">${data.phone_name}</h5>
+                    <p class="card-text">${data.brand}</p>
+                    <div>
+                    <button type="button" class="btn btn-lg btn-primary">See More...</button>
+                </div>
                 </div>
             </div>`;
         searchResult.appendChild(div)
