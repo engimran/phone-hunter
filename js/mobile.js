e27c01ac6e11dd01ce4782f042abdc296b7305df
Imran Hossen <imranhossen20@gmail.com>
2022-03-02 18:13:14 +0600

    commit

--- js/mobile.js
+++ js/mobile.js
@@ -17,7 +17,7 @@ const displayResult = data => {
         div.classList.add('col');
         div.innerHTML = `
             <div class="card">
-                <img class = "height= 300px w-50" src="${data.image}" class="card-img-top" alt="...">
+                <img class = "height= 300px w-25" src="${data.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${data.phone_name}</h5>
                     <p class="card-text">${data.brand}</p>
@@ -34,5 +34,17 @@ const seeMore = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => setSeeMore(data.data));
+}
+
+const setSeeMore = (info) => {
+    document.getElementById('details-container').innerHTML = `
+    <div class="d-flex" >
+    <img class = "w-25" src="${info.image}" class="card-img-top" alt="...">
+    <h3>Brand: ${info.brand}</h3>
+    
+    </div>
+
+
+    `
 }
